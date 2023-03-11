61d703e99d51d7d0dc06e77e53817f6acd611ebb
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 05:10:53 -0800

    FOUNDATIONS: Utility.Service Validation --> FAIL

--- tests/testthat/test-Utility.Service.R
+++ tests/testthat/test-Utility.Service.R
@@ -8,3 +8,31 @@ test_that('Utility.Service() return list of services',{
     is.list() |>
       expect_equal(TRUE)
 })
+
+# Prepend
+test_that('service instance has Prepend service',{
+  # Given
+  service <- Utility.Service()
+
+  # Then
+  service[['Prepend']] |>
+    is.null()         |>
+      expect_equal(FALSE)
+})
+test_that("field |> service[['Prepend']](string) Prepends string to field",{
+  # Given
+  service <- 
+    Utility.Broker()  |>
+    Utility.Service()
+
+  field  <- 'field'
+  string <- 'string'
+
+  # When
+  expected <- string |> paste(field, sep = '')
+
+  # Then
+  field |>
+    service[['Prepend']](string) |>
+      expect_equal(expected) 
+})
