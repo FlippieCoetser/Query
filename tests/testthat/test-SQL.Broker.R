916b9d14b049073553963c53a764cad8ef475c52
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 09:13:20 -0800

    BROKER: SQL.Broker LOWER --> FAIL

--- tests/testthat/test-SQL.Broker.R
+++ tests/testthat/test-SQL.Broker.R
@@ -37,3 +37,36 @@ test_that("field |> broker[['INCLOSE']]() Inclose field with square brackets",{
     broker[['INCLOSE']]() |>
       expect_equal(expected) 
 })
+
+# LOWER
+test_that('broker instance has LOWER operation',{
+  # Given
+  broker <- SQL.Broker()
+
+  # Then
+  broker[['LOWER']] |>
+    is.null()         |>
+      expect_equal(FALSE)
+})
+test_that("field |> broker[['LOWER']]() Inject field into lower() as alias SQL Statement",{
+  # Given
+  utilities <-
+    Utility.Broker() |> 
+    Utility.Service()
+
+  broker <- SQL.Broker()
+
+  field  <- 'field'
+  alias <- 'alias'
+
+  # When
+  expected <- field |> 
+    utilities[['Prepend']]('LOWER(') |> 
+    utilities[['Append']](') as ')   |> 
+    utilities[['Append']](alias)
+
+  # Then
+  field |>
+    broker[['LOWER']](alias) |>
+      expect_equal(expected) 
+})
