5c5fcbbf19bc6982a129cfdae94bccc5056f6529
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 05:01:48 -0800

    FOUNDATION: Utility.Validation Exist --> FAIL

--- tests/testthat/test-Utility.Validation.R
+++ tests/testthat/test-Utility.Validation.R
@@ -8,3 +8,23 @@ test_that('Utility.Validation() return list of validators',{
     is.list() |>
       expect_equal(TRUE)
 })
+
+# Exist Validator
+test_that("validator instance has Exist validator",{
+  # Given
+  validator <- Utility.Validation()
+
+  # Then
+  validator[["Exist"]]  |>
+    is.null()           |>
+      expect_equal(FALSE)
+})
+test_that("data |> validator[['Exist']]() should throw error when data is null",{
+  # Given
+  validator <- Utility.Validation()
+
+  # THEN
+  NULL |>
+    validator[['Exist']]() |>
+      expect_error()
+})
