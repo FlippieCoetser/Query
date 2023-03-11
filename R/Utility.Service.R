59c8a0ce41b83986e4dc3ff74d749b3f4a892ebe
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 05:11:12 -0800

    FOUNDATIONS: Utility.Service Validation --> PASS

--- R/Utility.Service.R
+++ R/Utility.Service.R
@@ -1,7 +1,18 @@
 Utility.Service <- \(broker) {
+  validate <- Utility.Validation()
+
   services <- list()
   services[['Prepend']] <- \(field, string) {
-    field |> broker[['Prepend']](string)
+    field |>
+      validate[['Exist']]() |>
+      validate[['IsCharacter']]()
+
+    string |>
+      validate[['Exist']]() |>
+      validate[['IsCharacter']]()
+
+    field |> 
+      broker[['Prepend']](string)
   }
   return(services)
 }
