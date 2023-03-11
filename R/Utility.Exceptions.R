928aa40b76361cda61bc0155751f72a7c7e39dd9
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 04:56:44 -0800

    FOUNDATIONS: Utility.Exceptions CharacterExceptions --> PASS

--- R/Utility.Exceptions.R
+++ R/Utility.Exceptions.R
@@ -3,5 +3,8 @@ Utility.Exceptions <- \() {
   exceptions[['NullException']]    <- \(invoke) {
     if (invoke) {stop('argument is NULL')}
   }
+  exceptions[['CharacterException']] <- \(invoke) {
+    if (invoke) {stop('argument is not of type character')}
+  }
   return(exceptions)
 }
