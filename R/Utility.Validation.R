5e94df640a92236b100779af5c7ae53349b8691f
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 05:05:21 -0800

    FOUNDATIONS: Utility.Validation IsCharacter --> PASS

--- R/Utility.Validation.R
+++ R/Utility.Validation.R
@@ -1,4 +1,12 @@
 Utility.Validation <- \() {
   validators <- list()
+  validators[['Exist']]     <- \(data) {
+    data |> is.null() |> exception[['NullException']]()
+    return(data)
+  }
+  validators[['IsCharacter']] <- \(data) {
+    data |> is.character() |> isFALSE() |> exception[['NumericException']]()
+    return(data)
+  }
   return(validators)
 }
