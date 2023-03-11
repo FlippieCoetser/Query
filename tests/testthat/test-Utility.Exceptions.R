9a141847b2d6293436a7fef22682f7d34ec36ae0
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 04:53:00 -0800

    FOUNDATIONS: Utility.Exceptions NullException --> FAIL

--- tests/testthat/test-Utility.Exceptions.R
+++ tests/testthat/test-Utility.Exceptions.R
@@ -8,3 +8,32 @@ test_that('Utility.Exceptions() return list of exceptions',{
     is.list() |>
       expect_equal(TRUE)
 })
+
+# Null Exception
+test_that("exception instance has NullException",{
+  # Given
+  exception <- Utility.Exceptions()
+
+  # Then
+  exception[['NullException']]  |>
+    is.null()              |>
+      expect_equal(FALSE)
+})
+test_that("FALSE |> exception[['NullException']]() throw no exception",{
+  # Given
+  exception <- Utility.Exceptions()
+
+  # Then
+  FALSE |>
+    exception[['NullException']]() |>
+      expect_no_error()
+})
+test_that("TRUE |> exception[['NullException']]() throw NULL exception",{
+  # Given
+  exception <- Utility.Exceptions()
+
+  # Then
+  TRUE |>
+    exception[['NullException']]() |>
+      expect_error('argument is NULL')
+})
