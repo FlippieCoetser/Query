97f4b5150bd5e70b4fc1d18d4082c5a60269e0db
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 08:52:27 -0800

    BROKER: SQL.Broker INCLOSE --> FAIL

--- tests/testthat/test-SQL.Broker.R
+++ tests/testthat/test-SQL.Broker.R
@@ -8,3 +8,32 @@ test_that("SQL.Broker() returns list of operations",{
     is.list()    |>
       expect_equal(TRUE)
 })
+
+# INCLOSE
+test_that('broker instance has INCLOSE operation',{
+  # Given
+  broker <- SQL.Broker()
+
+  # Then
+  broker[['INCLOSE']] |>
+    is.null()         |>
+      expect_equal(FALSE)
+})
+test_that("field |> broker[['INCLOSE']]() Inclose field with square brackets",{
+  # Given
+  utilities <-
+    Utility.Broker() |> 
+    Utility.Service()
+
+  broker <- SQL.Broker()
+
+  field  <- 'field'
+
+  # When
+  expected <- field |> utilities[['Prepend']]('[') |> utilities[['Prepend']](']')
+
+  # Then
+  field |>
+    broker[['INCLOSE']]() |>
+      expect_equal(expected) 
+})
