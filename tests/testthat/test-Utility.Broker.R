17de4f659ab8c3c4a1c1a0c8f9de24c718280cbf
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-11 04:23:58 -0800

    BROKER: Utility.Broker Prepend --> FAIL

--- tests/testthat/test-Utility.Broker.R
+++ tests/testthat/test-Utility.Broker.R
@@ -8,3 +8,29 @@ test_that('Utility.Broker() return list of operations',{
     is.list()      |>
       expect_equal(TRUE)
 })
+
+# Prepend
+test_that('broker instance has Prepend operation',{
+  # Given
+  broker <- Utility.Broker()
+
+  # Then
+  broker[['Prepend']] |>
+    is.null()         |>
+      expect_equal(FALSE)
+})
+test_that("field |> broker[['Prepend']](string) Prepends string to field",{
+  # Given
+  broker <- Utility.Broker()
+
+  field  <- 'field'
+  string <- 'string'
+
+  # When
+  expected <- string |> paste(field, sep = '')
+
+  # Then
+  field |>
+    broker[['Prepend']](string) |>
+      expect_equal(expected) 
+})
