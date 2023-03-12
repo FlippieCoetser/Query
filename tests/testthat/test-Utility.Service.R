9a4e34a41100eb4edc22d3b96968b76a08fedde6
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-12 01:19:29 -0800

    FOUNDATIONS: Utility.Service IncloseInSquareBrackets --> FAIL

--- tests/testthat/test-Utility.Service.R
+++ tests/testthat/test-Utility.Service.R
@@ -64,3 +64,32 @@ test_that("field |> service[['Append']](string) Append string to field",{
     service[['Append']](string) |>
       expect_equal(expected) 
 })
+
+# IncloseInSquareBrackets
+test_that('broker instance has IncloseInSquareBrackets service',{
+  # Given
+  service <- 
+    Utility.Broker()  |>
+    Utility.Service()
+
+  # Then
+  service[['IncloseInSquareBrackets']] |>
+    is.null()         |>
+      expect_equal(FALSE)
+})
+test_that("field |> service[['IncloseInSquareBrackets']]() inclose field in square brackets",{
+  # Given
+  service <- 
+    Utility.Broker()  |>
+    Utility.Service()
+
+  field  <- 'field'
+
+  # When
+  expected <- '[field]'
+
+  # Then
+  field |>
+    service[['IncloseInSquareBrackets']]() |>
+      expect_equal(expected) 
+})
