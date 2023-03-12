d9c6c25f626763d40516bd93c384a14b8b717c21
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-12 01:38:50 -0800

    PROCESSOR: Utility.Service Prepend --> FAIL

--- tests/testthat/test-Utility.Processing.R
+++ tests/testthat/test-Utility.Processing.R
@@ -8,3 +8,35 @@ test_that('Utility.Processing() return list of processors',{
     is.list() |>
       expect_equal(TRUE)
 })
+
+# Prepend
+test_that('processor instance has Prepend processor',{
+  # Given
+  processor <- 
+    Utility.Broker()  |>
+    Utility.Service() |>
+    Utility.Processing()
+
+  # Then
+  processor[['Prepend']] |>
+    is.null()          |>
+      expect_equal(FALSE)
+})
+test_that("field |> processor[['Prepend']](string) Prepends string to field",{
+  # Given
+  processor <- 
+    Utility.Broker()  |>
+    Utility.Service() |>
+    Utility.Processing()
+
+  field  <- 'field'
+  string <- 'string'
+
+  # When
+  expected <- string |> paste(field, sep = '')
+
+  # Then
+  field |>
+    processor[['Prepend']](string) |>
+      expect_equal(expected) 
+})
