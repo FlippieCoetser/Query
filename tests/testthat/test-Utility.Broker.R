e3986c149ee3cd7085b77afb238a001dfdc090f9
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-12 01:06:02 -0800

    BROKER: Utility.Broker IncloseInSquareBrackets --> FAIL

--- tests/testthat/test-Utility.Broker.R
+++ tests/testthat/test-Utility.Broker.R
@@ -60,3 +60,28 @@ test_that("field |> broker[['Append']](string) Append string to field",{
     broker[['Append']](string) |>
       expect_equal(expected) 
 })
+
+# IncloseInSquareBrackets
+test_that('broker instance has IncloseInSquareBrackets operation',{
+  # Given
+  broker <- Utility.Broker()
+
+  # Then
+  broker[['IncloseInSquareBrackets']] |>
+    is.null()         |>
+      expect_equal(FALSE)
+})
+test_that("field |> broker[['IncloseInSquareBrackets']](string) in close field in square brackets",{
+  # Given
+  broker <- Utility.Broker()
+
+  field  <- 'field'
+
+  # When
+  expected <- '[field]'
+
+  # Then
+  field |>
+    broker[['IncloseInSquareBrackets']]() |>
+      expect_equal(expected) 
+})
