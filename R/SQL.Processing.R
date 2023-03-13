8ff9ff698511092ac419801e61c529e85cd00316
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-13 12:31:32 -0700

    PROCESSING: SQL.Processing --> PASS

---
+++ R/SQL.Processing.R
@@ -0,0 +1,29 @@
+SQL.Processing <- \(service) {
+  processors <- list()
+  processors[['SELECT']] <- \(columns) {
+    service[['KEYWORDS']][['SELECT']]() |>
+    service[['UTILITIES']][['COLUMNS']](columns)
+  }
+  processors[['FROM']] <- \(input, table) {
+    input |>
+      service[['KEYWORDS']][['FROM']]() |>
+      service[['UTILITIES']][['SCHEMA']](table)
+  }
+  processors[['WHERE']] <- \(input, key, value) {
+    input |>
+      service[['KEYWORDS']][['WHERE']]() |>
+      service[['UTILITIES']][['KEYVALUE']](key, value)
+  }
+  processors[['INSERT']] <- \(table, columns) {
+    service[['KEYWORDS']][['INSERT']]()       |>
+    service[['KEYWORDS']][['INTO']]()         |>
+    service[['UTILITIES']][['SCHEMA']](table) |>
+    service[['UTILITIES']][['INCLOSELIST']](columns)
+  }
+  processors[['VALUES']] <- \(input, values) {
+    input |>
+      service[['KEYWORDS']][['VALUES']]() |>
+      service[['UTILITIES']][['INCLOSELIST']](values)
+  }
+  return(processors)
+}
