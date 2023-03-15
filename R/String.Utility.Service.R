3e1e3c777407de1376724600aba0f69e48e32404
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-15 13:32:16 -0700

    FOUNDATIONS: field |> service[['Append']](string) ThrowsExceptionIfFieldNull --> PASS

--- R/String.Utility.Service.R
+++ R/String.Utility.Service.R
@@ -1,6 +1,10 @@
 String.Utility.Service <- \(broker) {
+  validate <- String.Utility.Validation()
+  
   services <- list()
   services[['Append']] <- \(field, string) {
+    field |> validate[['Exist']]()
+    
     field |> broker[['Append']](string)
   }
   return(services)
