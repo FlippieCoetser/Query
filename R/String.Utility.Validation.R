0142e3f79e3b6ff6777a0b4551aa7c9d1ddc413e
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-16 07:04:43 -0700

    FOUNDATIONS: input |> validate[['Exist']]()  ReturnsInputIfInputNotNull --> PASS

--- R/String.Utility.Validation.R
+++ R/String.Utility.Validation.R
@@ -4,6 +4,7 @@ String.Utility.Validation <- \() {
   validators <- list()
   validators[['Exist']] <- \(input) {
     input |> is.null() |> exception[['NullException']]()
+    return(input)
   }
   validators[['IsString']] <- \(input) {
     input |> is.character() |> isFALSE() |> exception[['StringException']]()
