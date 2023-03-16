1cf351d3667c81e9258a3f8c167e59de150ebc01
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-16 03:52:11 -0700

    CODE RUB: Move Logic from Broker to Service

--- R/String.Utility.Broker.R
+++ R/String.Utility.Broker.R
@@ -1,7 +1,4 @@
 String.Utility.Broker <- \() {
   operations <- list()
-  operations[['Inclose']] <- \(field, before, after) {
-    before |> paste(field, sep = '') |> paste(after, sep = '')
-  }
   return(operations)
 }
--- tests/testthat/test-String.Utility.Broker.R
+++ tests/testthat/test-String.Utility.Broker.R
@@ -8,28 +8,3 @@ test_that('String.Utility.Broker() return list of operations',{
     is.list() |>
       expect_equal(TRUE)
 })
-
-# Inclose Operation
-test_that('broker instance has Inclose operation',{
-  # Given
-  broker <- String.Utility.Broker()
-
-  # Then
-  broker[['Inclose']] |>
-    Exist()          |>
-      expect_equal(TRUE)
-})
-test_that("field |> broker[['Inclose']](before, after) inclose field with before and after",{
-  # Given
-  broker <- String.Utility.Broker()
-
-  # When
-  field  <- 'field'
-  before <- 'before'
-  after  <- 'after'
-
-  # Then
-  field |>
-    broker[['Inclose']](before,after) |>
-      expect_equal('beforefieldafter')
-})
