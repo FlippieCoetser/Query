357c9287d2a646dadda62b2b9cd5d8c11b35591a
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-16 10:18:17 -0700

    FOUNDATIONS: service[['FIELDS']] Exist --> PASS

--- R/DQL.Service.R
+++ R/DQL.Service.R
@@ -2,12 +2,15 @@ DQL.Service <- \() {
   utilities <-
     String.Utility.Service() |>
     String.Utility.Processing()
-    
+
   services <- list()
   services[['SELECT']] <- \(input = '') {
     input |>
       utilities[['Append']]('SELECT') |>
       utilities[['Append']](' ')
+  }
+  services[['FIELDS']] <- \(input, fields) {
+    
   }
   return(services)
 }
