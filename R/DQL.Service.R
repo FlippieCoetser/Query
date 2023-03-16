fbf53c06080c3a411b867e769b11c4df1eebbd11
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-16 10:15:05 -0700

    FOUNDATIONS: service[['SELECT']]() ReturnsSELECTKeywordAndSpace --> PASS

--- R/DQL.Service.R
+++ R/DQL.Service.R
@@ -1,7 +1,13 @@
 DQL.Service <- \() {
-  services <- list()
-  services[['SELECT']] <- \(args) {
+  utilities <-
+    String.Utility.Service() |>
+    String.Utility.Processing()
     
+  services <- list()
+  services[['SELECT']] <- \(input = '') {
+    input |>
+      utilities[['Append']]('SELECT') |>
+      utilities[['Append']](' ')
   }
   return(services)
 }
