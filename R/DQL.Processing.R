8091f5c1d70ada6cd53cd5eb375f6752519e7157
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-17 06:24:28 -0700

    PROCESSING: processor[['SELECT']]() ReturnsSELECT_*_ --> PASS

--- R/DQL.Processing.R
+++ R/DQL.Processing.R
@@ -1,7 +1,8 @@
 DQL.Processing <- \(service) {
   processors <- list()
   processors[['SELECT']] <- \(fields) {
-    
+    service[['SELECT']]() |>
+    service[['*']]()
   }
   return(processors)
 }
