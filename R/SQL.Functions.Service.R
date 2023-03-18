1aee1fe973915456fe3ee96e1c9cad60b9648ecd
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-18 02:49:40 -0700

    FOUNDATIONS: value |> service[['LOWER']]() ReturnsLOWER(value) --> PASS

--- R/SQL.Functions.Service.R
+++ R/SQL.Functions.Service.R
@@ -1,7 +1,13 @@
 SQL.Functions.Service <- \() {
+  utilities <-
+    String.Utility.Service() |>
+    String.Utility.Processing()
+
   services <- list()
   services[['LOWER']] <- \(value) {
-    
+    value |>
+      utilities[['Prepend']]('LOWER(') |>
+      utilities[['Append']](')')
   }
   return(services)
 }
