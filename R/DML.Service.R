b6b986920343fb4129caef4c6e019e5c96d9d660
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-18 10:02:18 -0700

    FOUNDATIONS: input |> service[['INSERT']]() ReturnsINSERTPrependedInput --> PASS

--- R/DML.Service.R
+++ R/DML.Service.R
@@ -1,7 +1,13 @@
 DML.Service <- \() {
+  utilities <-
+    String.Utility.Service() |>
+    String.Utility.Processing()
+
   services <- list()
-  services[['INSERT']] <- \() {
-    
+  services[['INSERT']]   <- \(input = '') {
+    'INSERT' |>
+      utilities[['Append']](' ')   |>
+      utilities[['Prepend']](input)
   }
   return(services)
 }
