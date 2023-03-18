eadd5391373e274daae09a6bd531717e64886be6
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-18 08:22:41 -0700

    ACCEPTANCE: SELECT fields FROM table WHERE key = Value

--- tests/testthat/test-SQL.R
+++ tests/testthat/test-SQL.R
@@ -18,3 +18,26 @@ test_that('SELECT FROM Statements',{
     processor[['FROM']]('User') |>
       expect_equal(output)
 })
+test_that('SELECT FROM WHERE Statements',{
+  utilities <- SQL.Utility.Service()
+  functions <- SQL.Functions.Service()
+
+  processor <- DQL.Service() |> DQL.Processing()
+
+  fields <- list(
+    'Id'            |> utilities[['BRACKET']]() |> functions[['LOWER']]('Id'),
+    'Username'      |> utilities[['BRACKET']](),
+    'HashedPassword'|> utilities[['BRACKET']](),
+    'Salt'          |> utilities[['BRACKET']]() |> functions[['LOWER']]('Salt')
+  )
+  key   <- 'Id'
+  value <- 'b2970410-bd60-478d-baf6-46cbc14e10fc'
+
+  output <- "SELECT LOWER([Id]) as Id, [Username], [HashedPassword], LOWER([Salt]) as Salt FROM [dbo].[User] WHERE [Id] = 'b2970410-bd60-478d-baf6-46cbc14e10fc' "
+
+  fields |>
+    processor[['SELECT']]()          |>
+    processor[['FROM']]('User')      |>
+    processor[['WHERE']](key, value) |>
+      expect_equal(output)
+})
