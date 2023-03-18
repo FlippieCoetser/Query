ebe8bf42f82ccabcd1df4842b73307f5bdc343e6
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-18 02:58:17 -0700

    FOUNDATIONS: value |> service[['BRACKET']]() Returns[value] --> PASS

--- R/SQL.Utility.Service.R
+++ R/SQL.Utility.Service.R
@@ -1,7 +1,11 @@
 SQL.Utility.Service <- \() {
+  utilities <-
+    String.Utility.Service() |>
+    String.Utility.Processing()
+
   services <- list()
   services[['BRACKET']] <- \(value) {
-    
+    value |> utilities[['Inclose']]()
   }
   return(services)
 }
