dd39c99f213a4e132419a51ce2182447abc70463
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-24 08:54:50 -0700

    BROKER: Update Exception Messages

--- R/String.Utility.Exceptions.R
+++ R/String.Utility.Exceptions.R
@@ -4,10 +4,10 @@ String.Utility.Exceptions <- \() {
     if (invoke) {stop('argument is NULL')}
   }
   exceptions[['ArgumentNotCharacterException']] <- \(invoke) {
-    if (invoke) {stop('argument is not a String')}
+    if (invoke) {stop('argument is not of type Character')}
   }
   exceptions[['ArgumentNotVectorException']]    <- \(invoke) {
-    if (invoke) {stop('argument is not a Vector')}    
+    if (invoke) {stop('argument is not of type Vector')}    
   }
   return(exceptions)
 }
