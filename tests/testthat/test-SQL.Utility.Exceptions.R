a08cb27c6ce8560c28f029c93a93cf900779a60f
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-24 10:03:34 -0700

    FOUNDATIONS: exceptions is a list --> FAIL

--- tests/testthat/test-SQL.Utility.Exceptions.R
+++ tests/testthat/test-SQL.Utility.Exceptions.R
@@ -1,6 +1,16 @@
-describe("Given SQL.Utility.Exception",{
+describe("Given SQL.Utility.Exceptions",{
   it("exist",{
     # Given
     SQL.Utility.Exceptions |> is.null() |> expect_equal(FALSE)
+  })
+})
+
+describe("When exceptions <- SQL.Utility.Exceptions()",{
+  it("then exceptions is a list",{
+    # Given
+    exceptions <- SQL.Utility.Exceptions()
+
+    # Then
+    exceptions |> is.list() |> expect_equal(TRUE)
   })
 })
