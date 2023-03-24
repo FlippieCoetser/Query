583fdbc6cf6f22b57f6c45ba97b916a7a99747c6
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-24 07:15:37 -0700

    FOUNDATIONS: Update Exception References

--- R/String.Utility.Validation.R
+++ R/String.Utility.Validation.R
@@ -3,15 +3,15 @@ String.Utility.Validation <- \() {
 
   validators <- list()
   validators[['Exist']] <- \(input) {
-    input |> is.null() |> exception[['NullException']]()
+    input |> is.null() |> exception[['ArgumentNullException']]()
     return(input)
   }
   validators[['IsString']] <- \(input) {
-    input |> is.character() |> isFALSE() |> exception[['StringException']]()
+    input |> is.character() |> isFALSE() |> exception[['ArgumentNotCharacterException']]()
     return(input)
   }
   validators[['IsVector']] <- \(input) { 
-    input |> is.character() |> isFALSE() |> exception[['VectorException']]()
+    input |> is.character() |> isFALSE() |> exception[['ArgumentNotVectorException']]()
     return(input)
   }
   return(validators)
