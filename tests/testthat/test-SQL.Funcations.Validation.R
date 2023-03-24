8d3f6f899e5517f0e2b9f079098a464a7bdf7a1c
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-24 10:54:04 -0700

    FOUNDATIONS: ValidatorsIsList --> FAIL

--- tests/testthat/test-SQL.Funcations.Validation.R
+++ tests/testthat/test-SQL.Funcations.Validation.R
@@ -4,3 +4,13 @@ describe("Given SQL.Functions.Validation",{
     SQL.Functions.Validation |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("When validators <- SQL.Utility.Validation()",{
+  it("then validators is a list",{
+    # Given
+    validators <- SQL.Utility.Validation()
+    
+    # Then
+    validators |> is.list() |> expect_equal(TRUE)
+  })
+})
