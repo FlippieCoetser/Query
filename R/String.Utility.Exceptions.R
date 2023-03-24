b15a533b61e82d51af6cd6f42c58c0d894198b4f
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-24 07:18:51 -0700

    CODE RUB: Fix Indentation

--- R/String.Utility.Exceptions.R
+++ R/String.Utility.Exceptions.R
@@ -1,12 +1,12 @@
 String.Utility.Exceptions <- \() {
   exceptions <- list()
-  exceptions[['ArgumentNullException']] <- \(invoke) {
+  exceptions[['ArgumentNullException']]         <- \(invoke) {
     if (invoke) {stop('argument is NULL')}
   }
   exceptions[['ArgumentNotCharacterException']] <- \(invoke) {
     if (invoke) {stop('argument is not a String')}
   }
-  exceptions[['ArgumentNotVectorException']] <- \(invoke) {
+  exceptions[['ArgumentNotVectorException']]    <- \(invoke) {
     if (invoke) {stop('argument is not a Vector')}    
   }
   return(exceptions)
