5b39a8222152540e970233c0d221579005771cce
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-24 10:44:46 -0700

    FOUNDATIONS: ArgumentIsNullExceptionThrowsWhenInputTrue --> PASS

--- R/SQL.Functions.Exceptions.R
+++ R/SQL.Functions.Exceptions.R
@@ -1,6 +1,7 @@
 SQL.Functions.Exceptions <- \() {
   exceptions <- list()
   exceptions[['ArgumentNullException']] <- \(input) {
+    if(input) { stop('argument is NULL') }
   }
   return(exceptions)
 }
