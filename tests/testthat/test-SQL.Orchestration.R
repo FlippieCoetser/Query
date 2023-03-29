24d6c8b905548f9bec1d54d8b01e514054238f88
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-29 08:55:59 -0700

    ORCHESTRATION: OrchestrationsContainsSELECTOrchestration --> FAIL

--- tests/testthat/test-SQL.Orchestration.R
+++ tests/testthat/test-SQL.Orchestration.R
@@ -4,3 +4,20 @@ describe("Given SQL.Orchestration",{
     SQL.Orchestration |> is.null() |> expect_equal(FALSE)
   })
 })
+
+describe("When orchestration <- SQL.Orchestration()",{
+  it("then orchestration is a list",{
+    # Given
+    orchestrations <- SQL.Orchestration()
+
+    # Then
+    orchestrations |> is.list() |> expect_equal(TRUE)
+  })
+  it("then orchestration contains SELECT orchestration",{
+    # Given
+    orchestration <- SQL.Orchestration()
+
+    # Then
+    orchestration[['SELECT']] |> Exist() |> expect_equal(TRUE)
+  })
+})
