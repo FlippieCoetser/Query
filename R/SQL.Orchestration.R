117f47bd38b6e4852bdfffe269d02799d8c8ee1b
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-29 10:49:52 -0700

    ORCHESTRATION: Refactor Flow Control

--- R/SQL.Orchestration.R
+++ R/SQL.Orchestration.R
@@ -1,9 +1,17 @@
 SQL.Orchestration <- \(args) {
-  processor <- DQL.Service() |> DQL.Processing()
+  service   <- DQL.Service()
+  processor <- service |> DQL.Processing()
+  utilities <- SQL.Utility.Service() 
+
 
   orchestrations <- list()
   orchestrations[['SELECT']] <- \(fields = '*') {
-    processor[['SELECT']](fields) 
+
+    selections <- list()
+    selections[[1]] <- \() service[['SELECT']]() |> service[['*']]() 
+    selections[[2]] <- \() service[['SELECT']]() |> utilities[['FIELDS']](fields) 
+
+    selections[[(fields |> is.list()) + 1]]()    
   }
   return(orchestrations)
 }
