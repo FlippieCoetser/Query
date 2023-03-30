5cdb99ff0703d339adb6ef35bc0a0b9ea79486fd
Flippie Coetser <flippie.coetser@gmail.com>
2023-03-30 03:27:12 -0700

    CODE RUB: Rename Service Reference

--- R/SQL.Orchestration.R
+++ R/SQL.Orchestration.R
@@ -1,23 +1,25 @@
 SQL.Orchestration <- \(args) {
-  service   <- DQL.Service()
+  dql   <- DQL.Service()
   utilities <- SQL.Utility.Service() 
 
   orchestrations <- list()
   orchestrations[['SELECT']] <- \(fields = '*') {
     selections <- list()
-    selections[[1]] <- \() service[['SELECT']]() |> utilities[['*']]() 
-    selections[[2]] <- \() service[['SELECT']]() |> utilities[['FIELDS']](fields) 
+    selections[[1]] <- \() dql[['SELECT']]() |> utilities[['*']]() 
+    selections[[2]] <- \() dql[['SELECT']]() |> utilities[['FIELDS']](fields) 
     selections[[(fields |> is.list()) + 1]]()    
   }
   orchestrations[['FROM']]   <- \(input, table) {
     input |>
-      service[['FROM']]() |> utilities[['SCHEMA']]() |> utilities[['TABLE']](table)
+      dql[['FROM']]()        |> 
+      utilities[['SCHEMA']]()    |> 
+      utilities[['TABLE']](table)
   }
   orchestrations[['WHERE']] <- \(input, key, value) {
     input |>
-      service[['WHERE']]()       |>
-      utilities[['KEY']](key)    |>
-      utilities[['OPERATOR']]()  |>
+      dql[['WHERE']]()      |> 
+      utilities[['KEY']](key)   |> 
+      utilities[['OPERATOR']]() |> 
       utilities[['VALUE']](value) 
   }
   return(orchestrations)
